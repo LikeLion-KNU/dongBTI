a6cfc777933c32eeba99ee9f63a8117abff9ca54
toothlessdev <toothless042@gmail.com>
2024-08-26 23:38:33 +0900

    chore: 디렉토리 구조 및 vscode 설정

--- src/pages/HomePage.style.ts
+++ src/pages/HomePage.style.ts
@@ -3,6 +3,4 @@ import styled from "@emotion/styled";
 export const HomePageWrapper = styled.div`
     width: 100%;
     height: 100vh;
-
-    background-color: #000;
 `;
--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,5 +1,15 @@
+import { Text } from "@/components/typography/Text";
+
 import { HomePageWrapper } from "./HomePage.style";
 
 export default function HomePage() {
-    return <HomePageWrapper></HomePageWrapper>;
+    return (
+        <HomePageWrapper>
+            <Text size="xl">XL</Text>
+            <Text size="l">L</Text>
+            <Text size="m">M</Text>
+            <Text size="s">S</Text>
+            <Text size="xs">XS</Text>
+        </HomePageWrapper>
+    );
 }
