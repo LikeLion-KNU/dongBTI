848a474d514599e5d6e2db49a99a7e7eddc662e5
toothlessdev <toothless042@gmail.com>
2024-08-26 23:37:56 +0900

    chore: GlobalStyle 적용

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,14 @@
+import { BrowserRouter } from "react-router-dom";
+
 import { Router } from "./Router";
+import { GlobalStyle } from "./styles/global";
+import { Global } from "@emotion/react";
 
 export default function App() {
     return (
-        <>
+        <BrowserRouter>
+            <Global styles={GlobalStyle} />
             <Router />
-        </>
+        </BrowserRouter>
     );
 }
