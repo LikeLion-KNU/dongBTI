e85e761568ec1094d2f30a8dc1642c9514053f3b
Giwon <jkw04257773@gmail.com>
2024-08-28 21:15:01 +0900

    feat: 공유 페이지 개발중

--- src/Router.tsx
+++ src/Router.tsx
@@ -2,12 +2,14 @@ import { Route, Routes } from "react-router-dom";
 
 import { RootLayout } from "./components/layout/RootLayout";
 import HomePage from "./pages/HomePage";
+import ResultShare from "./pages/ResultShare";
 
 export const Router = () => {
     return (
         <Routes>
             <Route path="/" element={<RootLayout />}>
                 <Route index element={<HomePage />}></Route>
+                <Route path="/share" element={<ResultShare />}></Route>
             </Route>
         </Routes>
     );
