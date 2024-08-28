e345a9c80ea12f9f51889df258c9c65337d8642d
Buyeon Hwang <arpa1705@gmail.com>
2024-08-28 21:05:50 +0900

    feat: 결과보기 페이지 개발중

--- src/Router.tsx
+++ src/Router.tsx
@@ -2,12 +2,14 @@ import { Route, Routes } from "react-router-dom";
 
 import { RootLayout } from "./components/layout/RootLayout";
 import HomePage from "./pages/HomePage";
+import ResultPage from "./pages/ResultPage";
 
 export const Router = () => {
     return (
         <Routes>
             <Route path="/" element={<RootLayout />}>
                 <Route index element={<HomePage />}></Route>
+                <Route path="/result" element={<ResultPage />}></Route>
             </Route>
         </Routes>
     );
