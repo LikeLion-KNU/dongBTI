7f613c6d023e4f7e000f80bfb10aa1a5d045f3d8
2NNS-V <msy3824@knu.ac.kr>
2024-08-28 20:45:27 +0900

    feat:메인 페이지 퍼블리싱

--- src/pages/HomePage.style.ts
+++ src/pages/HomePage.style.ts
@@ -1,6 +1,63 @@
 import styled from "@emotion/styled";
 
 export const HomePageWrapper = styled.div`
+    display: flex;
+    flex-direction: column;
+    justify-content: space-between;
+    align-items: center; 
     width: 100%;
     height: 100vh;
 `;
+
+export const Header = styled.div`
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    text-align: center;
+    margin-top:50px;
+`;
+
+export const TextContainer = styled.div`
+    margin : 10px;
+`
+
+export const MiddleSection = styled.div`
+    flex-grow: 0.8;
+`
+
+export const Main = styled.div`
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+`
+
+export const InputContainer = styled.div`
+    display:flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    gap:15px;
+`
+
+export const ButtonContainer = styled.div`
+    display:flex;
+    justify-content: center;
+    align-items: center;
+
+    margin-top:50px;
+`
+
+export const Footer = styled.div`
+    display:flex;
+    justify-content: center;
+    align-items: center;
+    gap: 10px;
+    margin:20px;
+`
+
+export const MenuContainer = styled.div`
+    display:flex;
+    justify-content: flex-end;
+    align-items: flex-end;
+`
