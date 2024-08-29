53b39a15847a9a232e7cafd604ff3e9c97f6196e
WithJo <researcherDragon@gmail.com>
2024-08-29 22:59:53 +0900

    style: 선택 버튼에 맞는 스타일 추가 border 따로 구분

--- src/components/form/Button.style.ts
+++ src/components/form/Button.style.ts
@@ -5,8 +5,19 @@ export const ButtonElement = styled.button<ButtonProps>`
     width: ${(props) => props.width};
     height: ${(props) => props.height};
 
-    border: 1px solid #37cdcd;
     border-radius: 12px;
+
+    border: ${(props) => {
+        switch (props.variants) {
+            case "primary":
+                return "1px solid #37cdcd";
+            case "secondary":
+                return "1px solid #37cdcd";
+            case "select":
+                return "1px solid #ECECEC";
+        }
+    }};
+    // border: 1px solid #37cdcd; 혹시 몰라서 주석처리
 
     background-color: ${(props) => {
         switch (props.variants) {
@@ -14,6 +25,8 @@ export const ButtonElement = styled.button<ButtonProps>`
                 return "#37cdcd";
             case "secondary":
                 return "#fff";
+            case "select":
+                return "#ECECEC";
         }
     }};
 
@@ -23,6 +36,8 @@ export const ButtonElement = styled.button<ButtonProps>`
                 return "#fff";
             case "secondary":
                 return "#37cdcd";
+            case "select":
+                return "#707070";
         }
     }};
 `;
