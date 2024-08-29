8c01947b7e48fbf351c38e2df954e299451eeaa7
2NNS-V <msy3824@knu.ac.kr>
2024-08-29 11:46:01 +0900

    design:백그라운드 이미지 추가

--- src/components/layout/RootLayout.tsx
+++ src/components/layout/RootLayout.tsx
@@ -1,5 +1,7 @@
 import { Outlet } from "react-router-dom";
 
+import gradients from "@/assets/icons/gradients.svg";
+
 import styled from "@emotion/styled";
 
 export const Layout = styled.main`
@@ -7,6 +9,11 @@ export const Layout = styled.main`
 
     margin: 0px auto;
     padding: 1.25rem;
+
+    background-image: url(${gradients});
+    background-size: cover;
+    background-position: center;
+    background-repeat: no-repeat;
 `;
 
 export const RootLayout = () => {
