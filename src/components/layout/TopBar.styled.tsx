d48187b216bbe30b4892890ef94bd15649746ce6
Giwon <jkw04257773@gmail.com>
2024-08-29 12:55:06 +0900

    feat: 왼쪽 위 아이콘 대응

--- src/components/layout/TopBar.styled.tsx
+++ src/components/layout/TopBar.styled.tsx
@@ -1,20 +1,32 @@
 import styled from "@emotion/styled";
 
 export const TopBarWrapper = styled.div`
+    position: fixed;
+    top: 0;
+    left: 50%;
+    transform: translateX(-50%);
+
+    width: 100%;
     height: 60px;
+
+    // RootLayout의 max-width를 따라갑니다.
+    max-width: 800px;
 `;
 
 export const TopBarContainer = styled.div`
-    position: fixed;
-    top: 0;
-    left: 0;
-    right: 0;
+    position: relative;
+
     display: flex;
-    height: 56px;
+    height: 60px;
 
     justify-content: center;
     align-items: center;
 
     border-bottom: 1px solid #e0e0e0;
     font-weight: 800;
+
+    & > img {
+        position: absolute;
+        left: 15px;
+    }
 `;
