5d60e10d9c19e5ea711f773c1679a6c3cba76119
2NNS-V <msy3824@knu.ac.kr>
2024-09-02 14:14:53 +0900

    design:background image 삭제

--- src/components/layout/RootLayout.tsx
+++ src/components/layout/RootLayout.tsx
@@ -1,7 +1,5 @@
 import { Outlet } from "react-router-dom";
 
-import gradients from "@/assets/icons/gradients.svg";
-
 import styled from "@emotion/styled";
 
 export const Layout = styled.main`
@@ -9,11 +7,6 @@ export const Layout = styled.main`
 
     margin: 0px auto;
     padding: 1.25rem;
-
-    background-image: url(${gradients});
-    background-size: cover;
-    background-position: center;
-    background-repeat: no-repeat;
 `;
 
 export const RootLayout = () => {
