712269d4ea4f0eee2e73dc42196f4152be776a7e
2NNS-V <msy3824@knu.ac.kr>
2024-09-05 01:07:50 +0900

    refact: 불필요한 함수 제거(handleSideBar)

--- src/components/navigation/SideBar.tsx
+++ src/components/navigation/SideBar.tsx
@@ -1,50 +1,46 @@
 import React from "react";
-
-import {SideBarWrapper, Menu} from "./SideBar.style"
 import { useNavigate } from "react-router-dom";
+
 import closeIcon from "@/assets/icons/close.svg";
+
+import { SideBarWrapper, Menu } from "./SideBar.style";
 
 interface SideBarProps {
     isOpen: boolean;
     setIsOpen: React.Dispatch<React.SetStateAction<boolean>>;
 }
 
-export const SideBar = ({isOpen, setIsOpen} : SideBarProps) => {
+export const SideBar = ({ isOpen, setIsOpen }: SideBarProps) => {
     const outside = React.useRef<any>();
-    const navigate = useNavigate(); 
+    const navigate = useNavigate();
 
-    React.useEffect(() => {    
-        document.addEventListener('mousedown', handlerOutsie);    
-        return () => {      
-            document.removeEventListener('mousedown', handlerOutsie);    
-        };  
-    });   
-    
-    const handlerOutsie = (e: any) => {    
-        if (!outside.current.contains(e.target)) {      
-            handleSideBar();    
-        }  
-    };   
-    
-    const handleSideBar = () => {    
-        setIsOpen(false);  
+    React.useEffect(() => {
+        document.addEventListener("mousedown", handlerOutside);
+        return () => {
+            document.removeEventListener("mousedown", handlerOutside);
+        };
+    });
+
+    const handlerOutside = (e: any) => {
+        if (!outside.current.contains(e.target)) {
+            setIsOpen(false);
+        }
     };
 
     const handleNavigation = (path: string) => {
         navigate(path);
-        handleSideBar(); 
+        setIsOpen(false);
     };
 
-  return ( 
-      <SideBarWrapper id="sidebar" ref={outside} className={isOpen ? "open" : ""}>
-        <img src = {closeIcon} onClick = {handleSideBar}/>
+    return (
+        <SideBarWrapper id="sidebar" ref={outside} className={isOpen ? "open" : ""}>
+            <img src={closeIcon} onClick={() => setIsOpen(false)} />
 
-        <ul>
-          <Menu>테스트</Menu>
-          <Menu onClick={() => handleNavigation("/analytics")}>통계</Menu>
-          <Menu>만든이들</Menu>
-        </ul>
-      </SideBarWrapper>
-    )
-}
-
+            <ul>
+                <Menu>테스트</Menu>
+                <Menu onClick={() => handleNavigation("/analytics")}>통계</Menu>
+                <Menu>만든이들</Menu>
+            </ul>
+        </SideBarWrapper>
+    );
+};
