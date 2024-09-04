e7769a2709b062af33b8f70f30bccff8d787aa75
2NNS-V <msy3824@knu.ac.kr>
2024-09-04 21:58:37 +0900

    feat:통계 페이지 진행중

---
+++ src/pages/AnalyticsPage.style.ts
@@ -0,0 +1,27 @@
+import styled from "@emotion/styled";
+
+export const TitleContainer = styled.div`
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    text-align: center;
+    margin: 30px;
+`;
+
+export const TitleTop = styled.div`
+    display: flex;
+    width: 60%;
+    align-items: center;
+    justify-content: flex-start;
+`;
+
+export const Main = styled.div``;
+
+export const Content = styled.div`
+    margin: 20px;
+`;
+
+export const MiddleSection = styled.div`
+    flex-grow: 0.8;
+`;
--- src/pages/AnalyticsPage.tsx
+++ src/pages/AnalyticsPage.tsx
@@ -1,12 +1,77 @@
+import React from "react";
+import { useNavigate } from "react-router-dom";
+
+import Dropdown from "@/components/form/Dropdown";
 import { AppBar } from "@/components/navigation/AppBar";
+import { Text } from "@/components/typography";
 
-export default function AnalyzePage()  {
-  return (
-    <>
-    <AppBar children = "전체 통계" />
-    
-    </>
-    
-  )
+import backIcon from "@/assets/back.svg";
+
+import { TitleContainer, TitleTop, Main, Content, MiddleSection } from "./AnalyticsPage.style";
+
+export default function AnalyticsPage() {
+    const [selectedMajor, setSelectedMajor] = React.useState<string>("");
+    const navigate = useNavigate();
+
+    const handleGoBack = () => {
+        navigate("/");
+    };
+
+    return (
+        <>
+            <img src={backIcon} onClick={handleGoBack} />
+            <AppBar children="전체 통계" />
+
+            <TitleContainer>
+                <Text size="m">어떤 유형이 가장 많을까요?</Text>
+            </TitleContainer>
+
+            <Main>
+                <Content>
+                    <Text size="m">1위 </Text>
+                    <Text size="m">무대를 좋아하는 연주가형</Text>
+                </Content>
+
+                <Content>
+                    <Text size="m">2위 </Text>
+                    <Text size="m">극락왕생을 원하는 부처님형</Text>
+                </Content>
+                <Content>
+                    <Text size="m">3위 </Text>
+                    <Text size="m">티칭의 대가 현우진형</Text>
+                </Content>
+            </Main>
+            <MiddleSection />
+
+            <TitleContainer>
+                <TitleTop>
+                    <Dropdown
+                        color="secondary"
+                        width="100%"
+                        height="40px"
+                        selectedMajor={selectedMajor}
+                        setSelectedMajor={setSelectedMajor}
+                    />
+                    <Text size="m">에서</Text>
+                </TitleTop>
+
+                <Text size="m">어떤 유형이 가장 많을까요?</Text>
+            </TitleContainer>
+            <Main>
+                <Content>
+                    <Text size="m">1위 </Text>
+                    <Text size="m">무대를 좋아하는 연주가형</Text>
+                </Content>
+
+                <Content>
+                    <Text size="m">2위 </Text>
+                    <Text size="m">극락왕생을 원하는 부처님형</Text>
+                </Content>
+                <Content>
+                    <Text size="m">3위 </Text>
+                    <Text size="m">티칭의 대가 현우진형</Text>
+                </Content>
+            </Main>
+        </>
+    );
 }
-
