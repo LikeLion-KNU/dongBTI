df98a03d3e4f4eb68398f6d6489fafd0e8678bbc
WithJo <researcherDragon@gmail.com>
2024-09-05 13:07:57 +0900

    feat: Button클릭시 색상전환 애니메이션 추가

--- src/pages/SelectPage.tsx
+++ src/pages/SelectPage.tsx
@@ -20,6 +20,7 @@ export default function SelectPage() {
     let navigate = useNavigate();
 
     const [root, setRoot] = useState<TreeNode | null>(null);
+    const [selectedButton, setSelectedButton] = useState("");
     const field = [
         "전시,공연,연극 분야",
         "사회,자원활동 분야",
@@ -27,22 +28,6 @@ export default function SelectPage() {
         "체육,민족무예 분야",
         "학술,취미교양 분야",
     ];
-
-    function handleLeftClick() {
-        if (root?.left?.getValue.question === null) {
-            navigate(`/result?resultType=${root?.left?.getValue.resultType}`);
-        } else {
-            setRoot(root?.left || null);
-        }
-    }
-
-    function handleRightClick() {
-        if (root?.right?.getValue.question === null) {
-            navigate(`/result?resultType=${root?.right?.getValue.resultType}`);
-        } else {
-            setRoot(root?.right || null);
-        }
-    }
 
     function chooseSection(value: string) {
         switch (value) {
@@ -61,6 +46,30 @@ export default function SelectPage() {
         }
     }
 
+    function handleClick(e: React.MouseEvent<HTMLButtonElement>) {
+        const { id } = e.currentTarget; // 클릭된 버튼의 id 가져오기
+        setSelectedButton(id); // 클릭된 버튼 상태 저장
+
+        setTimeout(() => {
+            if (id === "left") {
+                if (root?.left?.getValue.question === null) {
+                    navigate(`/result?resultType=${root?.left?.getValue.resultType}`);
+                } else {
+                    setRoot(root?.left || null);
+                }
+            } else if (id === "right") {
+                if (root?.right?.getValue.question === null) {
+                    navigate(`/result?resultType=${root?.right?.getValue.resultType}`);
+                } else {
+                    setRoot(root?.right || null);
+                }
+            } else if (id.startsWith("root")) {
+                setRoot(chooseSection((e.target as HTMLButtonElement).innerText));
+            }
+            setSelectedButton(""); // 애니메이션 끝난 후 상태 초기화
+        }, 700); // 0.5초 후에 root 변경
+    }
+
     return (
         <>
             <SelectPageWrapper style={root === null ? { marginTop: "5vh" } : {}}>
@@ -75,21 +84,62 @@ export default function SelectPage() {
                 <ButtonWrapper style={root === null ? { gap: "2.5vh" } : {}}>
                     {root ? (
                         <>
-                            <Button width="80%" height="7vh" variants="select" onClick={handleLeftClick}>
+                            <Button
+                                id="left"
+                                width="80%"
+                                height="7vh"
+                                variants="select"
+                                onClick={handleClick}
+                                style={
+                                    selectedButton === "left"
+                                        ? {
+                                              backgroundColor: "#37cdcd",
+                                              color: "#FFFF",
+                                              transition: "background-color 0.3s, color 0.3s",
+                                          }
+                                        : undefined
+                                }
+                            >
                                 <Text size="m">{root?.left?.getValue.choice}</Text>
                             </Button>
-                            <Button width="80%" height="7vh" variants="select" onClick={handleRightClick}>
+                            <Button
+                                id="right"
+                                width="80%"
+                                height="7vh"
+                                variants="select"
+                                onClick={handleClick}
+                                style={
+                                    selectedButton === "right"
+                                        ? {
+                                              backgroundColor: "#37cdcd",
+                                              color: "#FFFF",
+                                              transition: "background-color 0.3s, color 0.3s",
+                                          }
+                                        : undefined
+                                }
+                            >
                                 <Text size="m">{root?.right?.getValue.choice}</Text>
                             </Button>
                         </>
                     ) : (
                         <>
-                            {field.map((value) => (
+                            {field.map((value, index) => (
                                 <Button
+                                    key={index}
+                                    id={`root${index}`}
                                     width="80%"
                                     height="7vh"
                                     variants="select"
-                                    onClick={() => setRoot(chooseSection(value))}
+                                    onClick={handleClick}
+                                    style={
+                                        selectedButton === `root${index}`
+                                            ? {
+                                                  backgroundColor: "#37cdcd",
+                                                  color: "#FFFF",
+                                                  transition: "background-color 0.3s, color 0.3s",
+                                              }
+                                            : undefined
+                                    }
                                 >
                                     <Text size="m">{value}</Text>
                                 </Button>
