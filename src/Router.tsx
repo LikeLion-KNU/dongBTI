724eafcb3b3e3a7c2aa49b02106c0b934e8681a6
WithJo <researcherDragon@gmail.com>
2024-09-05 10:29:46 +0900

    chore: route수정

--- src/Router.tsx
+++ src/Router.tsx
@@ -2,24 +2,20 @@ import { Route, Routes } from "react-router-dom";
 
 import { RootLayout } from "./components/layout/RootLayout";
 import HomePage from "./pages/HomePage";
-import SelectPage from "./pages/SelectPage";
-
-import LoadingPage from './pages/ResultLoading';
-import ResultShare from "./pages/ResultShare";
+import LoadingPage from "./pages/ResultLoading";
 import ResultPage from "./pages/ResultPage";
-
+import ResultShare from "./pages/ResultShare";
+import SelectPage from "./pages/SelectPage";
 
 export const Router = () => {
     return (
         <Routes>
             <Route path="/" element={<RootLayout />}>
                 <Route index element={<HomePage />}></Route>
-                <Route path="/loading" element={<LoadingPage />} /> 
+                <Route path="/loading" element={<LoadingPage />} />
                 <Route path="/result" element={<ResultPage />}></Route>
                 <Route path="/share" element={<ResultShare />}></Route>
-            </Route>
-            <Route path="/select" element={<RootLayout />}>
-                <Route index element={<SelectPage />}></Route>
+                <Route path="/select" element={<SelectPage />}></Route>
             </Route>
         </Routes>
     );
