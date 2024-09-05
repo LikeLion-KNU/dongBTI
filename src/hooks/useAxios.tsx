6f96cd1ce1d7034679cfa8a087fbb31cb2e79de4
2NNS-V <msy3824@knu.ac.kr>
2024-09-05 23:29:55 +0900

    feat: Axios 훅 추가

---
+++ src/hooks/useAxios.tsx
@@ -0,0 +1,37 @@
+import React from "react";
+
+import axios from "axios";
+import { AxiosRequestConfig } from "axios";
+
+const instance = axios.create({
+    baseURL: "https://api.dongbti.com",
+    withCredentials: true,
+});
+
+export default function useAxios<T>(config: AxiosRequestConfig, dependencies: any[] = []) {
+    const [data, setData] = React.useState<T | null>(null);
+    const [loading, setLoading] = React.useState<boolean>(false);
+    const [error, setError] = React.useState<string | null>(null);
+
+    React.useEffect(() => {
+        const fetchData = async () => {
+            setLoading(true);
+            try {
+                const response = await instance(config);
+                setData(response.data);
+                console.log(response.data);
+            } catch (err: any) {
+                if (axios.isAxiosError(err)) {
+                    setError(err.message || "Something went wrong");
+                } else {
+                    setError("Something went wrong");
+                }
+            } finally {
+                setLoading(false);
+            }
+        };
+        fetchData();
+    }, dependencies);
+
+    return { data, loading, error };
+}
