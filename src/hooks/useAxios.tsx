508574d5afdcf140f4b54d0c505956bf71612904
2NNS-V <msy3824@knu.ac.kr>
2024-09-05 23:58:00 +0900

    refact: useAxios 타입 명시

--- src/hooks/useAxios.tsx
+++ src/hooks/useAxios.tsx
@@ -8,7 +8,10 @@ const instance = axios.create({
     withCredentials: true,
 });
 
-export default function useAxios<T>(config: AxiosRequestConfig, dependencies: any[] = []) {
+export default function useAxios<T>(
+    config: AxiosRequestConfig,
+    dependencies: ReadonlyArray<React.DependencyList> = [],
+) {
     const [data, setData] = React.useState<T | null>(null);
     const [loading, setLoading] = React.useState<boolean>(false);
     const [error, setError] = React.useState<string | null>(null);
