86531225c617cbde531dc28f4ecba71a67143395
Giwon <jkw04257773@gmail.com>
2024-09-09 18:30:42 +0900

    refacter: 사이드바 디자인 자연스럽게 수정 + 헤더 이미지 추가

--- src/components/navigation/SideBar.tsx
+++ src/components/navigation/SideBar.tsx
@@ -2,8 +2,9 @@ import React, { useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 
 import closeIcon from "@/assets/icons/close.svg";
+import headerImage from "@/assets/images/sidabar_header.png";
 
-import { SideBarWrapper, Menu } from "./SideBar.style";
+import { SideBarWrapper, Menu, Backdrop } from "./SideBar.style";
 
 interface SideBarProps {
     isOpen: boolean;
@@ -28,19 +29,23 @@ export const SideBar = ({ isOpen, setIsOpen }: SideBarProps) => {
     };
 
     const handleNavigation = (path: string) => {
-        navigate(path);
         setIsOpen(false);
+        navigate(path);
     };
 
     return (
-        <SideBarWrapper id="sidebar" ref={outside} className={isOpen ? "open" : ""}>
-            <img src={closeIcon} onClick={() => setIsOpen(false)} />
+        <>
+            <SideBarWrapper id="sidebar" ref={outside} className={isOpen ? "open" : ""}>
+                <img src={closeIcon} className="close" onClick={() => setIsOpen(false)} />
+                <img src={headerImage} />
 
-            <ul>
-                <Menu onClick={() => handleNavigation("/")}>테스트</Menu>
-                <Menu onClick={() => handleNavigation("/analytics")}>통계</Menu>
-                <Menu onClick={() => handleNavigation("/credits")}>만든이들</Menu>
-            </ul>
-        </SideBarWrapper>
+                <ul>
+                    <Menu onClick={() => handleNavigation("/")}>테스트</Menu>
+                    <Menu onClick={() => handleNavigation("/analytics")}>통계</Menu>
+                    <Menu onClick={() => handleNavigation("/credits")}>만든이들</Menu>
+                </ul>
+            </SideBarWrapper>
+            <Backdrop className={isOpen ? "open" : ""} />
+        </>
     );
 };
