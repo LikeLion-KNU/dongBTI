5b251f8c2648f51ae96841bf7b0ef41e5682f034
Giwon <jkw04257773@gmail.com>
2024-09-09 18:31:24 +0900

    refacter: 만든이들 디자인 자연스럽게 수정 (PC/태블릿 대응)

--- src/components/display/DeveloperProfile.tsx
+++ src/components/display/DeveloperProfile.tsx
@@ -27,8 +27,10 @@ const Container = styled.div`
     background-color: white;
     padding: 20px;
     border-radius: 20px;
-    box-shadow: 2px 2px 10px rgba(0, 0, 0, 0.1);
-    margin-bottom: 20px;
+    box-shadow: 2px 2px 30px rgba(0, 0, 0, 0.06);
+
+    flex-grow: 1;
+    flex-basis: 0;
 
     $ > img {
         border-radius: 10px;
@@ -37,14 +39,15 @@ const Container = styled.div`
 `;
 
 const Image = styled.img`
-    width: 84px;
-    height: 84px;
+    width: 73px;
+    height: 73px;
     border-radius: 10px;
 `;
 
 const InfoContainer = styled.div`
     display: flex;
     gap: 16px;
+    align-items: center;
 `;
 
 const InfoDiv = styled.div`
@@ -99,10 +102,10 @@ export const DeveloperProfile: React.FC<DeveloperCardProps> = ({ info }) => {
                 <Image src={`https://github.com/${info.github_id}.png`} />
                 <InfoDiv>
                     <NameWorkDiv>
-                        <Text size="l" weight="extrabold">
+                        <Text size="m" weight="extrabold">
                             {info.name}
                         </Text>
-                        <Text size="xs" color="#818181">
+                        <Text size="12px" color="#818181">
                             {info.worklist.join(" · ")}
                         </Text>
                     </NameWorkDiv>
