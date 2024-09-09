083252a03a039ebec157dd974a075aceba46bd31
toothlessdev <toothless042@gmail.com>
2024-09-10 03:00:55 +0900

    feat: react-toastify 설정

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,6 @@
 import { BrowserRouter } from "react-router-dom";
+import { Bounce, ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 import { FontsStyle } from "@/styles/fonts";
 import { GlobalStyle } from "@/styles/global";
@@ -9,6 +11,19 @@ import { Global } from "@emotion/react";
 export default function App() {
     return (
         <BrowserRouter>
+            <ToastContainer
+                position="top-center"
+                autoClose={3000}
+                hideProgressBar={false}
+                newestOnTop={false}
+                closeOnClick
+                rtl={false}
+                pauseOnFocusLoss
+                draggable
+                pauseOnHover
+                theme="light"
+                transition={Bounce}
+            />
             <Global styles={[FontsStyle, GlobalStyle]} />
             <Router />
         </BrowserRouter>
