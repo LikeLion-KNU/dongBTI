52aaf60f3211d17be9e7dad9e991535538f31f1f
2NNS-V <msy3824@knu.ac.kr>
2024-09-09 22:06:15 +0900

    refact: 공통 interface 지정 및 변수 이름 통일

--- src/pages/AnalyticsPage.tsx
+++ src/pages/AnalyticsPage.tsx
@@ -1,6 +1,7 @@
 import { useState, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 
+import Footer from "@/components/display/Footer";
 import DropDown from "@/components/form/DropDown";
 import TopBar from "@/components/layout/TopBar";
 import { Text } from "@/components/typography";
@@ -12,11 +13,11 @@ import backIcon from "@/assets/back.svg";
 
 import { TitleContainer, TitleTop, Main, MiddleSection, TableContainer, Card, Rank, Type } from "./AnalyticsPage.style";
 
-interface axiosProps {
+export interface top10Response {
     top: [string, number][];
 }
 
-const renderData = (data: axiosProps | null, isPending: boolean, isError: any) => {
+const renderData = (data: top10Response | null, isPending: boolean, isError: any) => {
     if (isPending) return <Text size="m">로딩중...</Text>;
     if (isError) return <Text size="m">오류가 발생했습니다.</Text>;
     if (!data) {
@@ -49,11 +50,11 @@ export default function AnalyticsPage() {
         navigate("/");
     };
 
-    const { top10, isPending: Top10Loading, isError: Top10Error } = useTop10();
+    const { top10, isPending: top10Loading, isError: top10Error } = useTop10();
     const {
         top10ByDepartment,
-        isPending: Top10ByDepartmentLoading,
-        isError: Top10ByDepartmentError,
+        isPending: top10ByDepartmentLoading,
+        isError: top10ByDepartmentError,
     } = useTop10ByDepartment(selectedDepartment);
 
     useEffect(() => {
@@ -68,10 +69,12 @@ export default function AnalyticsPage() {
             <TopBar title="전체 통계" />
 
             <TitleContainer>
-                <Text size="m">어떤 유형이 가장 많을까요?</Text>
+                <Text size="m" weight="bold">
+                    어떤 유형이 가장 많을까요?
+                </Text>
             </TitleContainer>
 
-            <Main>{renderData(top10, Top10Loading, Top10Error)}</Main>
+            <Main>{renderData(top10, top10Loading, top10Error)}</Main>
 
             <MiddleSection />
 
@@ -84,12 +87,19 @@ export default function AnalyticsPage() {
                         selectedDepartment={selectedDepartment}
                         setSelectedDepartment={setSelectedDepartment}
                     />
-                    <Text size="m">에서</Text>
+                    <Text size="m" weight="bold" style={{ marginLeft: "10px" }}>
+                        에서
+                    </Text>
                 </TitleTop>
 
-                <Text size="m">어떤 유형이 가장 많을까요?</Text>
+                <Text size="m" weight="bold">
+                    어떤 유형이 가장 많을까요?
+                </Text>
             </TitleContainer>
-            <Main>{renderData(top10ByDepartment, Top10ByDepartmentLoading, Top10ByDepartmentError)}</Main>
+            <Main>
+                {selectedDepartment && renderData(top10ByDepartment, top10ByDepartmentLoading, top10ByDepartmentError)}
+            </Main>
+            <Footer />
         </>
     );
 }
