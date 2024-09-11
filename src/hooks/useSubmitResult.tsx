6ca3ec9757c4fa2a1ec76c152a6c74996ccfff21
WithJo <researcherDragon@gmail.com>
2024-09-11 11:02:15 +0900

    chore: useState 삭제

--- src/hooks/useSubmitResult.tsx
+++ src/hooks/useSubmitResult.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import { useEffect } from "react";
 import { useNavigate, useSearchParams } from "react-router-dom";
 
 import { api } from "@/config/axios";
